7125f0dafdb42a0680b8d19e88795bb1e7dce6ea
ArifFadilah <ariffadilah16@gmail.com>
2021-06-29 12:34:49 +0700

    user management system

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+    <h1>hi</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Admin\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+//admin routes
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function (){
+    Route::resource('/users', UserController::class);
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
